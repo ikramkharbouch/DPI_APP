c3cad7264389a7f3517070e80a36b3d013c2ea85
Ikram <4573r14@gmail.com>
2021-09-21 16:46:17 +0100

    Add responsive form

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -1,15 +1,15 @@
 const Menu = () => {
     return (<>
-        <div className="w-11/12 bg-white h-20 mx-auto mt-20 rounded-lg flex justify-around items-center">
-            <h1 className="text-3xl text-blue-400 ml-20">DPI APP</h1>
-            <ul className="flex items-center justify-around mr-20 text-gray-500 font-medium">
-                <li className="mr-10">Accueil</li>
-                <li className="mr-10">A propos de nous</li>
+        <div className="w-5/6 lg:w-11/12 bg-white h-20 mx-auto mt-20 rounded-lg flex justify-around items-center text-center">
+            <h1 className="text-3xl text-blue-500">DPI APP</h1>
+            <ul className="flex items-center justify-around text-gray-500 font-medium px-5">
+                <li className="mr-4">Accueil</li>
+                <li className="mr-4">A propos de nous</li>
                 <li className="">Aide</li>
             </ul>
             <div className="flex flex-end">
-                <button className="mr-10 text-blue-400 font-medium">Button 1</button>
-                <button className="bg-blue-300 px-5 py-3 rounded-md text-white font-bold">Button 2</button>
+                <button className=" mr-4 text-blue-400 font-medium">S'inscrire</button>
+                <button className="bg-blue-500 px-5 py-3 rounded-md text-white font-bold">S'identifier</button>
             </div>
         </div>
     </>);
