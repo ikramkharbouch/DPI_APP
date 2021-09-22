c67ee30f3c4a7d70422c35a937d5f24dfd8c1003
Ikram <4573r14@gmail.com>
2021-09-22 16:44:51 +0100

    Adjust responsivity for mobile

--- client/pages/login.js
+++ client/pages/login.js
@@ -27,16 +27,16 @@ const Login = () => {
                     <li className="hover:bg-gray-700 bg-blue-500 mx-auto w-1/3 py-4 rounded mt-4">S'identifier</li>
                 </ul>
             </div>
-            <div className="w-5/6 lg:w-11/12 h-2/5 lg:h-3/4 bg-white mx-auto mt-4 lg:mt-20 rounded-lg flex flex-col lg:flex-row justify-between items-center px-4 lg:px-32 gap">
+            <div className="w-5/6 lg:w-11/12 h-3/5 lg:h-3/4 bg-white mx-auto mt-4 lg:mt-20 rounded-lg flex flex-col lg:flex-row justify-between items-center lg:px-32 gap">
                 <div className="hidden lg:block w-1/2"><Image src={medicalRecord} width={1000}
                     height={1000} layout="intrinsic" alt="Picture of the medical record" /></div>
-                <div className="w-full lg:w-96 h-5/6 lg:h-3/6 lg:bg-gray-200 rounded-lg mx-auto text-center mt-10">
-                    <form className="grid mt-20 justify-items-stretch w-3/4 mx-auto text-center">
-                        <input type="text" className="py-5 rounded mr-10 w-full border lg:border-0" placeholder="Addresse e-mail" />
-                        <input type="text" className="mt-5 py-5 rounded border lg:border-0" placeholder="Mot de passe" />
+                <div className="w-full lg:w-96 h-5/6 lg:h-3/6 lg:bg-gray-200 rounded-lg mx-auto text-center lg:mt-10">
+                    <form className="grid mt-20 lg:mt-20 justify-items-stretch w-3/4 mx-auto text-center">
+                        <input type="text" className="px-6 py-2 lg:mt-5 lg:py-5 rounded border lg:w-full lg:border-0" placeholder="E-mail" />
+                        <input type="text" className="px-6 py-2 mt-5 lg:py-5 rounded border lg:w-full lg:border-0" placeholder="Mot de passe" />
                         <button className="mt-5 font-medium text-blue-500 justify-self-end">Mot de passe oublier ?</button>
 
-                        <input className="px-28 py-5 rounded bg-blue-500 mt-14 font-bold text-white" type="submit"></input>
+                        <input className="lg:px-28 py-2 lg:py-5 rounded bg-blue-500 mt-14 font-bold text-white" type="submit"></input>
                     </form>
                 </div>
             </div>
