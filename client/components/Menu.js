b823a071bfbc88d03109465f394ab7af6667ecf2
Ikram <4573r14@gmail.com>
2021-09-22 18:26:56 +0100

    Update menu + login

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
 
 const Menu = ({ showMenu }) => {
 
@@ -7,13 +8,13 @@ const Menu = ({ showMenu }) => {
         <div className="w-5/6 lg:w-11/12 bg-white h-20 mx-auto mt-20 rounded-lg flex justify-around items-center text-center">
             <h1 className="text-3xl text-blue-500 flex-start font-bold">DPI APP</h1>
             <ul className="items-center justify-around text-gray-500 font-medium px-5 hidden lg:flex">
-                <li className="mr-4">Accueil</li>
-                <li className="mr-4">A propos de nous</li>
-                <li className="">Aide</li>
+                <Link href="/home"><a className="mr-4">Accueil</a></Link>
+                <Link href="/about"><a className="mr-4">A propos de nous</a></Link>
+                <Link href="/help"><a className="">Aide</a></Link>
             </ul>
-            <div className="hidden lg:flex flex-end">
-                <button className=" mr-4 text-blue-400 font-medium">S'inscrire</button>
-                <button className="bg-blue-500 px-5 py-3 rounded-md text-white font-bold">S'identifier</button>
+            <div className="hidden lg:flex flex-end items-center">
+                <Link href="/register"><a><button className="mr-4 text-blue-400 font-medium">S'inscrire</button></a></Link>
+                <Link href="/login"><a><button className="bg-blue-500 px-5 py-3 rounded-md text-white font-bold">S'identifier</button></a></Link>
             </div>
             <FontAwesomeIcon icon={faBars} color="gray" onClick={showMenu} size="lg" className='block lg:hidden' />
         </div>
--- client/pages/login.js
+++ client/pages/login.js
@@ -30,13 +30,15 @@ const Login = () => {
             <div className="w-5/6 lg:w-11/12 h-3/5 lg:h-3/4 bg-white mx-auto mt-4 lg:mt-20 rounded-lg flex flex-col lg:flex-row justify-between items-center lg:px-32 gap">
                 <div className="hidden lg:block w-1/2"><Image src={medicalRecord} width={1000}
                     height={1000} layout="intrinsic" alt="Picture of the medical record" /></div>
-                <div className="w-full lg:w-96 h-5/6 lg:h-3/6 lg:bg-gray-200 rounded-lg mx-auto text-center lg:mt-10">
-                    <form className="grid mt-10 lg:mt-11 justify-items-stretch w-3/4 mx-auto text-center">
-                        <input type="text" className="px-6 py-2 lg:mt-5 lg:py-5 rounded border lg:w-full lg:border-0" placeholder="E-mail" />
-                        <input type="text" className="px-6 py-2 mt-5 lg:py-5 rounded border lg:w-full lg:border-0" placeholder="Mot de passe" />
-                        <button className="mt-5 font-medium text-blue-500 justify-self-end">Mot de passe oublier ?</button>
+                <div className="w-full lg:w-3/5 h-5/6 lg:h-4/6 lg:bg-gray-200 rounded-lg mx-auto text-center lg:mt-10">
+                    <form className="max-w-full grid mt-20 lg:mt-11 justify-items-stretch w-3/4 mx-auto text-center p-0 m-0">
 
-                        <input className="lg:px-28 py-2 lg:py-5 rounded bg-blue-500 mt-14 font-bold text-white" type="submit" value="S'identifier" />
+                        <input type="text" className="py-2 mt-5 lg:py-5 rounded border lg:border-0" placeholder="E-mail" />
+                        <input type="password" className="py-2 mt-5 lg:py-5 rounded border lg:border-0" placeholder="Mot de passe" />
+
+                        <button className="mt-5 font-medium text-blue-500 justify-self-end">Avez vous déjà un compte ?</button>
+
+                        <input className="min-w-full lg:px-28 py-2 lg:py-5 rounded bg-blue-500 mt-14 font-bold text-white" type="submit" value="S'identifier" />
                     </form>
                 </div>
             </div>
