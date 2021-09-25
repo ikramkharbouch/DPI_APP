5abaafef949331a6921c4db2798dea04d7f0ea8d
Ikram <4573r14@gmail.com>
2021-09-25 11:57:31 +0100

    Update register submit

--- client/pages/register.js
+++ client/pages/register.js
@@ -6,6 +6,14 @@ import Footer from '../components/Footer'
 
 const Register = () => {
 
+    // The state variables
+
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [repeatPassword, setRepeatPassword] = useState("")
+    
+
     const [hiddenMenu, sethiddenMenu] = useState("hidden")
 
     const showMenu = () => {
@@ -14,6 +22,28 @@ const Register = () => {
             sethiddenMenu("block");
         else
             sethiddenMenu("hidden")
+    }
+
+    const handleForm = async (e) => {
+
+        e.preventDefault()
+
+        const data = {
+            username, email, password, repeatPassword
+        }
+
+        const res = await fetch('http://localhost:8000/user/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              },
+            body: JSON.stringify(data)
+        })
+
+        const formattedRes = await res.json()
+
+        console.log(formattedRes)
     }
 
     return (<>
@@ -32,14 +62,14 @@ const Register = () => {
                 <div className="hidden lg:block w-1/2"><Image src={medicalRecord} width={1000}
                     height={1000} layout="intrinsic" alt="Picture of the medical record" /></div>
                 <div className="w-full lg:w-3/5 h-5/6 lg:h-4/6 lg:bg-gray-200 rounded-lg mx-auto text-center lg:mt-10">
-                    <form className="max-w-full grid mt-20 lg:mt-11 justify-items-stretch w-3/4 mx-auto text-center p-0 m-0">
+                    <form className="max-w-full grid mt-20 lg:mt-11 justify-items-stretch w-3/4 mx-auto text-center p-0 m-0" onSubmit={handleForm}>
 
-                        <input type="text" className="py-2 mt-5 lg:py-5 rounded border lg:border-0" placeholder="Nom et prénom" />
-                        <input type="text" className="py-2 mt-5 lg:py-5 rounded border lg:border-0" placeholder="E-mail" />
-                        <input type="password" className="py-2 mt-5 lg:py-5 rounded border lg:border-0" placeholder="Mot de passe" />
-                        <input type="password" className="py-2 mt-5 lg:py-5 rounded border lg:border-0" placeholder="Confirmez votre mot de passe" />
+                        <input type="text" className="py-2 pl-5 mt-5 lg:py-5 rounded border lg:border-0" placeholder="Nom d'utilisateur" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <input type="text" className="py-2 mt-5 pl-5 lg:py-5 rounded border lg:border-0" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        <input type="password" className="py-2 mt-5 pl-5 lg:py-5 rounded border lg:border-0" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="password" className="py-2 mt-5 pl-5 lg:py-5 rounded border lg:border-0" placeholder="Confirmez votre mot de passe" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)}/>
 
-                        <button className="text-xs lg:text-base mt-5 font-medium text-blue-500 justify-self-end">Avez vous déjà un compte ?</button>
+                        <button className="text-xs md:text-base lg:text-base mt-5 font-medium text-blue-500 justify-self-end">Avez vous déjà un compte ?</button>
 
                         <input className="min-w-full lg:px-28 py-2 lg:py-5 rounded bg-blue-500 mt-14 font-bold text-white" type="submit" value="S'inscrire" />
                     </form>
