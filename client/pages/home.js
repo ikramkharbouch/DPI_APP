18a79f5b938a8c10e7df21c71cc913505d6778c7
Ikram <4573r14@gmail.com>
2021-09-28 17:19:06 +0100

    Add appointment's form

--- client/pages/home.js
+++ client/pages/home.js
@@ -4,13 +4,15 @@ import { faSearch, faPlus, faCalendar } from '@fortawesome/free-solid-svg-icons'
 import ModalBox from '../components/ModalBox'
 import { useState, useEffect } from 'react'
 import withAuth from './auth/withAuth'
+import Calendar from '../components/Calendar'
 
 
 const Home = () => {
 
     const [hiddenModal, setHiddenModal] = useState("hidden")
+    const [hiddenCalendar, setHiddenCalendar] = useState("hidden")
 
-    const doSomething = () => {
+    const addPatient = () => {
         if (hiddenModal === "hidden")
             setHiddenModal("block")
         else if (hiddenModal === "block")
@@ -19,7 +21,6 @@ const Home = () => {
     }
 
     const closeBox = () => {
-
         if (hiddenModal === "block")
             setHiddenModal("hidden")
         else if (hiddenModal === "hidden")
@@ -27,10 +28,27 @@ const Home = () => {
         console.log(hiddenModal)
     }
 
+    const closeCalendar = () => {
+        if (hiddenCalendar === "block")
+            setHiddenCalendar("hidden")
+        else if (hiddenCalendar === "hidden")
+            setHiddenCalendar("block")
+        console.log(hiddenCalendar)
+    }
+
+    const setAppointment = () => {
+        if (hiddenCalendar === "block")
+            setHiddenCalendar("hidden")
+        else if (hiddenCalendar === "hidden")
+            setHiddenCalendar("block")
+        
+    }
+
     return (<>
         <div className="h-screen mx-auto text-center relative">
             <LoggedinMenu />
-            <ModalBox classnames={hiddenModal} onClick={doSomething} closeBox={closeBox} />
+            <ModalBox classnames={hiddenModal} onClick={addPatient} closeBox={closeBox} />
+            <Calendar classnames={hiddenCalendar} closeCalendar={closeCalendar} />
             <div className="w-5/6 lg:w-11/12 h-3/5 lg:h-3/4 bg-white mx-auto mt-4 lg:mt-20 rounded-lg items-center lg:px-32 gap">
                 <div className="pt-10 w-11/12 flex mx-auto text-center justify-between items-center">
                     <form className="w-full ml-0">
@@ -38,13 +56,13 @@ const Home = () => {
                         <FontAwesomeIcon icon={faSearch} color="gray" size="lg" className="relative right-16 md:right-24 lg:right-32 mt-5" />
                     </form>
                     <div className="mr-20 mx-auto lg:mx-0 hidden lg:flex">
-                        <button className="px-4 py-2 lg:px-7 lg:py-4 rounded bg-green-300 text-white font-bold mr-5 mt-5 lg:mt-0">Add Patient</button>
-                        <button className="px-4 py-2 lg:px-7 lg:py-4 rounded bg-blue-300 text-white font-bold">Set Calendar</button>
+                        <button className="px-4 py-2 lg:px-7 lg:py-4 rounded bg-green-300 text-white font-bold mr-5 mt-5 lg:mt-0" onClick={addPatient}>Add Patient</button>
+                        <button className="px-4 py-2 lg:px-7 lg:py-4 rounded bg-blue-300 text-white font-bold" onClick={setAppointment}>Set Appointment</button>
                     </div>
 
                     <div className="flex lg:hidden gap-2 mx-auto mr-auto">
-                        <div className="bg-green-300 w-16 h-16 rounded-full text-center"><FontAwesomeIcon icon={faPlus} color="white" size="lg" className="mt-5 cursor-pointer" onClick={doSomething} /></div>
-                        <div className="bg-blue-400 w-16 h-16 rounded-full text-center"><FontAwesomeIcon icon={faCalendar} color="white" size="lg" className="mt-5 cursor-pointer" /></div>
+                        <div className="bg-green-300 w-16 h-16 rounded-full text-center"><FontAwesomeIcon icon={faPlus} color="white" size="lg" className="mt-5 cursor-pointer" onClick={addPatient} /></div>
+                        <div className="bg-blue-400 w-16 h-16 rounded-full text-center"><FontAwesomeIcon icon={faCalendar} color="white" size="lg" className="mt-5 cursor-pointer" onClick={setAppointment}/></div>
                     </div>
 
                 </div>
