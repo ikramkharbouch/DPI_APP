2d3ae099bb24adeec15a57ff959fdb5833d135ef
Ikram <4573r14@gmail.com>
2021-09-29 15:20:09 +0100

    Update

--- client/components/Calendar.js
+++ client/components/Calendar.js
@@ -1,7 +1,6 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
-import Input from './Input'
 
 
 const Calendar = ({ classnames, closeCalendar }) => {
@@ -18,15 +17,21 @@ const Calendar = ({ classnames, closeCalendar }) => {
     }
 
     return (<>
-        <div className={`${classnames} text-3xl text-center bg-white h-2/5 md:h-4/6 lg:h-3/6 w-5/6 z-10 rounded mx-auto shadow-2xl absolute ml-auto mr-auto left-0 right-0 mt-14`}>
+        <div className={`${classnames} text-3xl text-center bg-white h-3/5 md:h-4/6 lg:h-3/6 w-5/6 z-10 rounded mx-auto shadow-2xl absolute ml-auto mr-auto left-0 right-0 mt-14`}>
             <FontAwesomeIcon icon={faTimes} color="red" onClick={closeCalendar} size="xs" className='cursor-pointer float-right mr-10 mt-5' />
             <h1 className="text-gray-600 text-xl mt-10 font-bold">Set Appointment</h1>
             <div className="mx-auto text-center">
                 <form className="flex flex-col flex-wrap mx-auto" onSubmit={SubmitHandler}>
-                    <div className="px-10 flex flex-col gap-6 lg:gap-10 mx-auto pt-10 w-4/5" >
-                    <Input type="text" label="Appointment's title" placeholder="Ikram's appointment" classnames="w-full" value={title} onChange={(e) => setTitle(e.target.value)}/>
-                    <Input type="time" label="Timing" placeholder="Ikram" classnames="w-full pl-5" value={timing} onChange={(e) => setTiming(e.target.value)}/>
-                    <input type="submit" className="w-3/6 md:w-2/6 lg:w-1/6 bg-green-400 text-sm lg:text-lg py-2 lg:py-4 text-white font-bold rounded mt-5" />
+                    <div className="px-10 flex flex-col gap-6 lg:gap-10 mx-auto pt-10 w-full" >
+                    <div>
+                            <label className="text-xs text-blue-500 absolute font-bold">Appointment's title</label>
+                            <input type="text" placeholder="Some random title" className="pl-5 py-2 mt-5 lg:py-5 rounded border text-sm md:w-40 lg:w-40 w-full" />
+                        </div>
+                        <div>
+                            <label className="text-xs text-blue-500 absolute font-bold">Timing</label>
+                            <input type="time" placeholder="Ikram" className="py-2 mt-5 lg:py-5 rounded border text-sm md:w-40 lg:w-40 w-full pl-5" />
+                        </div>
+                    <input type="submit" className="w-3/6 md:w-2/6 lg:w-1/6 bg-green-400 text-sm lg:text-lg py-2 lg:py-4 text-white font-bold rounded mt-5 cursor-pointer" />
                     </div>
                 </form>
             </div>
--- client/components/ModalBox.js
+++ client/components/ModalBox.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Input from './Input'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
@@ -22,13 +21,14 @@ const ModalBox = ({ classnames, closeBox }) => {
     const [PhoneNumber, setPhoneNumber] = useState("")
     const [BloodType, setBloodType] = useState("")
     const [Vaccinated, setVaccinated] = useState(true)
-    const [medicalAntecedents, setMedicalAntecedents] = useState([])
+    const [vaccinationDate, setVaccinationDate] = useState()
+    const [medicalAntecedents, setMedicalAntecedents] = useState(Tags)
 
     const KeyPressHandler = (e) => {
         const Keycode = e.which
         if (Keycode === 13) {
             setTag(e.target.value)
-            if (!Tags.includes(Tag)) {
+            if (!Tags.includes(Tag) && Tag.length <= 12) {
                 setTags([...Tags, Tag])
                 setTagsNumber(tagsNumber + 1)
             }
@@ -46,37 +46,72 @@ const ModalBox = ({ classnames, closeBox }) => {
 
     const SubmitHandler = (e) => {
         e.preventDefault();
+        console.log(e.which)
         if (e.which === 13) {
             console.log("I won't handle the form")
         }
         else {
+            console.log(firstName)
+            console.log(middleName)
+            console.log(lastName)
+            console.log(birthDate)
+            console.log(MaritalStatus)
+            console.log(Nationality)
+            console.log(PhoneNumber)
+            console.log(BloodType)
+            console.log(Vaccinated)
+            console.log(vaccinationDate)
+            console.log(medicalAntecedents)
             console.log("It will handle the form")
         }
     }
 
-    const removeTag = () => {
-        console.log("We should remove the tag in here")
+    const removeTag = (e) => {
+        var poppedTag = e.target.parentElement.id
+
+        const newTags = Tags.filter((tag) => tag !== poppedTag)
+
+        console.log(newTags)
     }
 
 
     const [numberRegion, setNumberRegion] = useState('+212')
 
     return (<>
-        <div className={`${classnames} text-3xl text-center bg-white h-4/5 md:h-3/6 lg:h-3/6 w-5/6 z-10 rounded mx-auto shadow-2xl absolute ml-auto mr-auto left-0 right-0 mt-14`}>
+        <div className={`${classnames} text-3xl text-center bg-white h-11/12 mb-10 md:h-3/6 lg:h-3/6 w-5/6 z-10 rounded mx-auto shadow-2xl absolute ml-auto mr-auto left-0 right-0 mt-14`}>
             <FontAwesomeIcon icon={faTimes} color="red" onClick={closeBox} size="xs" className='cursor-pointer float-right mr-10 mt-5' />
             <h1 className="text-gray-600 text-xl mt-10 font-bold">Add patient</h1>
             <div className="mx-auto text-center">
                 <form className="flex flex-col flex-wrap mx-auto" onSubmit={SubmitHandler} >
                     <div className="px-10 flex flex-col md:flex-row lg:flex-row gap-4 lg:gap-10 mx-auto pt-10 w-full">
-                        <Input type="text" label="First Name" placeholder="Ikram" classnames="w-full" />
-                        <Input type="text" label="Middle Name" placeholder="Aicha" classnames="w-full" />
-                        <Input type="text" label="Last Name" placeholder="Kharbouch" classnames="w-full" />
-                        <Input type="date" label="Birth Date" placeholder="29-10-1998" classnames="w-full" />
+
+                        <div>
+                            <label className="text-xs text-blue-500 absolute font-bold">First Name</label>
+                            <input type="text" placeholder="Ikram" className="py-2 mt-5 lg:py-5 rounded border text-sm md:w-40 lg:w-40 w-full pl-5" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                        </div>
+
+                        <div>
+                            <label className="text-xs text-blue-500 absolute font-bold">Middle Name</label>
+                            <input type="text" placeholder="Aicha" className="py-2 mt-5 lg:py-5 rounded border text-sm md:w-40 lg:w-40 pl-5 w-full" value={middleName} onChange={(e) => setMiddleName(e.target.value)} />
+
+                        </div>
+
+                        <div>
+                            <label className="text-xs text-blue-500 absolute font-bold">Last Name</label>
+                            <input type="text" placeholder="Ikram" className="py-2 mt-5 lg:py-5 rounded border text-sm md:w-40 lg:w-40 pl-5 w-full" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+
+                        </div>
+
+                        <div>
+                            <label className="text-xs text-blue-500 absolute font-bold">Birth Date</label>
+                            <input type="date" placeholder="Ikram" className="py-2 mt-5 lg:py-5 rounded border text-sm md:w-40 lg:w-40 pl-5 w-full" value={birthDate} onChange={(e) => setBirthDate(e.target.value)}/>
+
+                        </div>
                     </div>
                     <div className="px-10 flex flex-col md:flex-row lg:flex-row gap-4 lg:gap-10 mx-auto pt-10 w-full">
                         <div>
                             <label className="text-xs text-blue-500 absolute font-bold">Marital status</label>
-                            <select className="py-2 mt-5 lg:py-5 rounded border text-sm md:w-40 lg:w-40 text-gray-400 w-full">
+                            <select className="py-2 mt-5 lg:py-5 rounded border text-sm md:w-40 lg:w-40 pl-5 w-full" value={MaritalStatus} onChange={(e) => setMaritalStatus(e.target.value)}>
                                 <option value="single">Single</option>
                                 <option value="married">Married</option>
                             </select>
@@ -84,7 +119,7 @@ const ModalBox = ({ classnames, closeBox }) => {
 
                         <div>
                             <label className="text-xs text-blue-500 absolute font-bold">Nationality</label>
-                            <select className="py-2 mt-5 lg:py-5 rounded border text-sm w-full md:w-40 lg:w-40 text-gray-400">
+                            <select className="py-2 mt-5 lg:py-5 rounded border text-sm w-full md:w-40 lg:w-40 pl-5" value={Nationality} onChange={(e) => setNationality(e.target.value)}>
                                 <option value="single">Morocco</option>
                                 <option value="married">France</option>
                             </select>
@@ -96,12 +131,12 @@ const ModalBox = ({ classnames, closeBox }) => {
                         <div className="flex flex-col">
                             <label className="text-xs text-blue-500 font-bold absolute">Phone Number</label>
                             <div className="w-12 h-5  bg-yellow-200 text-yellow-400 relative top-7 lg:top-10 left-5 text-xs rounded-full font-bold">{numberRegion}</div>
-                            <input type="text" className="py-2 lg:py-5 rounded border text-sm w-60 pl-20" placeholder="0655481976..." id="phone-number" />
+                            <input type="text" className="py-2 lg:py-5 rounded border text-sm w-60 pl-20" placeholder="0655481976..." id="phone-number" value={PhoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
                         </div>
 
                         <div>
                             <label className="text-xs text-blue-500 absolute font-bold">Blood Type</label>
-                            <select className="py-2 mt-5 lg:py-5 rounded border text-sm w-full md:w-40 lg:w-40 text-gray-400">
+                            <select className="py-2 mt-5 lg:py-5 rounded border text-sm w-full md:w-40 lg:w-40 pl-5" value={BloodType} onChange={(e) => setBloodType(e.target.value)}>
                                 <option value="single">AB+</option>
                                 <option value="married">A+</option>
                                 <option value="married">B+</option>
@@ -111,7 +146,7 @@ const ModalBox = ({ classnames, closeBox }) => {
 
                         <div>
                             <label className="text-xs text-blue-500 absolute font-bold">Covid19 Vaccinated</label>
-                            <select className="py-2 mt-5 lg:py-5 rounded border text-sm w-full md:w-40 lg:w-40 text-gray-400">
+                            <select className="py-2 mt-5 lg:py-5 rounded border text-sm w-full md:w-40 lg:w-40 pl-5" value={Vaccinated} onChange={(e) => setVaccinated(e.target.value)}>
                                 <option value="single">No</option>
                                 <option value="married">Yes</option>
                             </select>
@@ -119,14 +154,18 @@ const ModalBox = ({ classnames, closeBox }) => {
                     </div>
 
                     <div className="px-10 flex flex-col md:flex-row lg:flex-row gap-4 lg:gap-10 mx-auto pt-10 w-full">
-                        <Input type="date" label="Vaccination Date" placeholder="29-10-1998" classnames="w-full md:w-40 lg:w-40" />
+                        <div>
+                            <label className="text-xs text-blue-500 absolute font-bold">Vaccination Date</label>
+                            <input type="date" className="py-2 mt-5 lg:py-5 rounded border text-sm md:w-40 lg:w-40 pl-5 w-full" />
+
+                        </div>
                         <div className="flex flex-col w-full md:w-4/5 lg:w-4/5">
                             <label className="text-xs font-bold text-left text-blue-500">Medical Antecedents</label>
-                            <div className="input border flex items-center w-full rounded h-10 lg:h-16 self-end mt-1">
-                                <ul className="text-sm flex flex-wrap">
-                                    {Tags.map((tag) => <div className="flex"><li key={tag} className="bg-green-100 px-6 md:py-1 rounded-full border text-green-400 font-bold border-green-400 ml-2 text-xs">#{tag}<FontAwesomeIcon icon={faTimes} color="gray" onClick={removeTag} size="sm" className='ml-1 absolute mt-0.5' /></li></div>)}
+                            <div className="input border flex items-center w-full rounded h-14 lg:h-16 self-end mt-1">
+                                <ul className="text-sm flex flex-wrap gap-2">
+                                    {Tags.map((tag) => <div className="flex"><li key={tag} id={tag} className="bg-green-100 px-6 md:py-1 rounded-full border text-green-400 font-bold border-green-400 ml-2 text-xs">#{tag}<FontAwesomeIcon key={tag} id={tag} icon={faTimes} color="gray" onClick={removeTag} size="sm" className='ml-1 absolute mt-0.5 cursor-pointer' /></li></div>)}
                                 </ul>
-                                {(tagsNumber < 3) && <input type="text" placeholder="Enter here..." className="text-sm ml-5" value={Tag} onChange={ChangeHandler} onKeyPress={KeyPressHandler} />}
+                                {(tagsNumber < 5) && <input type="text" placeholder="Enter here..." className="text-sm ml-5 pl-5" value={Tag} onChange={ChangeHandler} onKeyPress={KeyPressHandler} />}
                             </div>
                         </div>
 
