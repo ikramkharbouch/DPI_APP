10e0c874902787cadc0ecdad1cbfe83d76f3f29c
Ikram <4573r14@gmail.com>
2021-10-22 13:37:23 +0100

    Fix the styling of the landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const homePage = () => {
 
     return (<>
-
         <Navbar bg="light" expand="lg">
             <Container className="mx-24">
                 <Navbar.Brand href="#home" className="text-primary font-bold">DPI APP</Navbar.Brand>
@@ -20,17 +19,17 @@ const homePage = () => {
         </Navbar>
 
 
-        <Container className="flex justify-around lg:justify-between items-center md:items-start lg:items-start first-section" style={{ height: "40%" }}>
-            <Container className="py-4">
-                <h1 className="text-xl lg:text-3xl md:mt-11 lg:mt-28 ml-0">Créez votre <span className="text-blue-500 font-bold">dossier
+        <Container className="flex justify-around lg:justify-between items-center md:items-start lg:items-start first-section" style={{ height: "50%" }}>
+            <Container className="py-4 mt-20">
+                <h1 className="text-xl lg:text-3xl ml-0">Créez votre <span className="text-blue-500 font-bold">dossier
                     patient en ligne</span></h1>
-                <p className="md:mt-5 lg:mt-10 text-xs md:text-sm lg:text-base">Using our software, one can book, create, and follow their medical records easily and without using any paper.
+                <p className="text-xs md:text-sm lg:text-base md:mt-3 lg:mt-5">Using our software, one can book, create, and follow their medical records easily and without using any paper.
                     You can use our product easily by registering and approving your email.
                     Using our software, one can book, create, and follow their medical records easily and without using any paper.
                     You can use our product easily by registering and approving your email.</p>
                 <Button variant="primary" className=" md:mt-4 lg:mt-10 btn-bg">S'inscrire</Button>
             </Container>
-            <Image src="doctor-lady-5.png" className="w-1/2 h-full hidden md:block lg:block first-image" />
+            <Image src="doctor-lady-5.png" className="w-full h-full hidden md:block lg:block first-image" />
         </Container>
 
         <Container className="w-full bg-blue-500 mt-0 hidden md:flex lg:flex text-white gap-50 mx-auto" style={{ height: "120px" }}>
@@ -65,25 +64,25 @@ const homePage = () => {
                     <div className="w-1/4 shadow-lg mt-10 rounded-lg h-full flex items-center" style={{ height: "16rem" }}>
                         <div className="mx-10">
                             <h1 className="md:text-sm lg:text-base">Procédure <span className="font-bold text-blue-500">Facile</span></h1>
-                            <p className="font-light md:text-xs lg:text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy.</p>
+                            <p className="font-light md:text-xs text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy.</p>
                         </div>
                     </div>
                     <div className="w-1/4 shadow-lg mt-10 rounded-lg h-full flex items-center" style={{ height: "16rem" }}>
                         <div className="mx-10">
                             <h1 className="md:text-sm lg:text-base">Rendez-<span className="font-bold text-blue-500">Vous</span></h1>
-                            <p className="font-light md:text-xs lg:text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy.</p>
+                            <p className="font-light md:text-xs text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy.</p>
                         </div>
                     </div>
                     <div className="w-1/4 shadow-lg mt-10 rounded-lg h-full flex items-center" style={{ height: "16rem" }}>
                         <div className="mx-10">
                             <h1 className="md:text-sm lg:text-base">Sécurité des <span className="font-bold text-blue-500">données</span></h1>
-                            <p className="font-light md:text-xs lg:text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy.</p>
+                            <p className="font-light md:text-xs text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy.</p>
                         </div>
                     </div>
                     <div className="w-1/4 shadow-lg mt-10 rounded-lg h-full flex items-center" style={{ height: "16rem" }}>
                         <div className="mx-10">
                             <h1 className="md:text-sm lg:text-base">Sécurité des <span className="font-bold text-blue-500">données</span></h1>
-                            <p className="font-light md:text-xs lg:text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy.</p>
+                            <p className="font-light md:text-xs text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy.</p>
                         </div>
                     </div>
                 </div>
